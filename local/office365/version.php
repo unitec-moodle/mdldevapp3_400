44ceb3a05d255e85be9ecb2ce3fa4fd259d2b0a6
unitec-moodle <mhay2@unitec.ac.nz>
2023-08-24 16:44:02 +1200

    instal local plugin office365

---
+++ local/office365/version.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version information.
+ *
+ * @package local_office365
+ * @author James McQuillan <james.mcquillan@remote-learner.net>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright (C) 2014 onwards Microsoft, Inc. (http://microsoft.com/)
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2021051735;
+$plugin->requires = 2021051700;
+$plugin->release = '3.11.6';
+$plugin->component = 'local_office365';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = [
+    'auth_oidc' => 2021051735,
+    'block_microsoft' => 2021051720,
+    'local_o365' => 2021051735,
+    'repository_office365' => 2021051730,
+    'theme_boost_o365teams' => 2021051720,
+];
